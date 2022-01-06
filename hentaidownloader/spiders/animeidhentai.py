7ef7ad7d8109912ebf23ad6e377ebd9f9e93cfd6
issei <geekerandseeker444@gmail.com>
2022-01-06 23:33:43 +0530

    hentai list viewing is enabled in terminal

--- hentaidownloader/spiders/animeidhentai.py
+++ hentaidownloader/spiders/animeidhentai.py
@@ -5,12 +5,12 @@ class AnimeidhentaiSpider(scrapy.Spider):
     name = 'animeidhentai'
     allowed_domains = ['animeidhentai.com']
     # start_urls = ['https://animeidhentai.com/']
-
+    
     def start_requests(self):
         print('Choose the options below : \n 1.Latest Hentai \n 2.Uncensored Hentai \n 3.Choose a Genre')
         option = int(input('Enter Option Number Here :) - '))
         if option == 1 :
-            yield scrapy.Request(url='https://animeidhentai.com/year/2021/',callback=self.parse,meta={'option':option})
+            yield scrapy.Request(url='https://animeidhentai.com/',callback=self.parse,meta={'option':option})
         if option == 2 :
             yield scrapy.Request(url='https://animeidhentai.com/genre/hentai-uncensored/',callback=self.parse,meta={'option':option})
         if option == 3 :
@@ -21,37 +21,47 @@ class AnimeidhentaiSpider(scrapy.Spider):
     def parse(self,response):
         option = response.request.meta['option']
         if option == 1 :
-            hentais=response.xpath("//a[@class='lnk-blk']")
+            hentai_list={}
+            refer_hentai=0
+            hentais=response.xpath("//div[@class='tb-bx mgt']//a[@class='lnk-blk']")
             for hentai in hentais:
                 title=hentai.xpath('.//@aria-label').get()
                 # title=response.xpath('//div[@class="ttl fz14 link-co"]/text()').get()
                 link=hentai.xpath('.//@href').get()
-                
-                yield{
-                    title,
-                    link
-                }
+                hentai_list.update({refer_hentai:[title,link]})
+                refer_hentai+=1
+
+            for n in range(len(hentai_list)):
+                print(str(n)+'.'+hentai_list[n][0])
+            hentai_chosen=int(input('Enter Desired Hentai Number from list :) -'))
+            yield scrapy.Request(url=hentai_list[int(hentai_chosen)][1],callback=self.video_parse)
         elif option == 2:
+            hentai_list={}
+            refer_hentai=0
             hentais=response.xpath("//a[@class='lnk-blk']")
             for hentai in hentais:
                 title=hentai.xpath('.//@aria-label').get()
                 # title=response.xpath('//div[@class="ttl fz14 link-co"]/text()').get()
                 link=hentai.xpath('.//@href').get()
-    
-                yield{
-                    'title':title,
-                    'source_link':link
-                }
+                hentai_list.update({refer_hentai:[title,link]})
+                refer_hentai+=1
+
+            for n in range(len(hentai_list)):
+                print(str(n)+'.'+hentai_list[n][0])
+            hentai_chosen=int(input('Enter Desired Hentai Number from list :) -'))
+            yield scrapy.Request(url=hentai_list[int(hentai_chosen)][1],callback=self.video_parse)
                 
     def genre_parse(self,response):
         list=response.xpath(".//section[7]//a[@class='lnk-blk']")
         genre_list={}
         refer_genre=0
+        
         for genres in list:
             genre=genres.xpath('.//@aria-label').get()
             genre_link=genres.xpath('.//@href').get()
             genre_list.update({refer_genre:[genre,genre_link]})
             refer_genre+=1
+
         for n in range(len(genre_list)):
             print(str(n)+'.'+genre_list[n][0])
         genre_chosen=input('Enter Desired Genre Number from list :) -')
@@ -59,17 +69,24 @@ class AnimeidhentaiSpider(scrapy.Spider):
             
     def parse_genre_list(self,response):
         hentais=response.xpath("//a[@class='lnk-blk']")
+        refer_hentai=0
+        hentai_list={}
+
         for hentai in hentais:
             title=hentai.xpath('.//@aria-label').get()
             # title=response.xpath('//div[@class="ttl fz14 link-co"]/text()').get()
             link=hentai.xpath('.//@href').get()
+            hentai_list.update({refer_hentai:[title,link]})
+            refer_hentai+=1
 
-            yield{
-                'title':title,
-                'source_link':link
-            }
-    
+        for n in range(len(hentai_list)):
+            print(str(n)+'.'+hentai_list[n][0])
+        hentai_chosen=int(input('Enter Desired Hentai Number from list :) -'))
+        yield scrapy.Request(url=hentai_list[int(hentai_chosen)][1],callback=self.video_parse)
     ###################  Now Parsing Videos  #######################
+    def video_parse(self,response):
+        print(response)
+
 
         
 
